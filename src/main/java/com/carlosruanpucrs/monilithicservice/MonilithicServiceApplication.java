fe57da9397780eaa57e3fbb8b83b06e3d34440b7
Carlos Gomes <carlos.gomes@agi.com.br>
2024-03-28 01:30:40 -0300

    projeto criado

---
+++ src/main/java/com/carlosruanpucrs/monilithicservice/MonilithicServiceApplication.java
@@ -0,0 +1,13 @@
+package com.carlosruanpucrs.monilithicservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonilithicServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonilithicServiceApplication.class, args);
+	}
+
+}
